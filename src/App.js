16d87a7b8ba65a4d2821a2994e152dd1e395a583
Hasan Mahmud <hasansujon786@gmail.com>
2019-09-04 16:53:51 -0700

    add header comp and links to Home & Shop page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import React from 'react'
 import './App.css'
 import HomePage from './pages/home/home.comp'
 import Shop from './pages/shop/shop.comp'
+import Header from './components/header/header.comp'
 
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/shop'>Shop</Link>
-              </li>
-              <li>
-                <Link to='/users/'>Users</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Route path='/' exact component={HomePage} />
-          <Route path='/shop' component={Shop} />
-        </div>
-      </Router>
+      <Header></Header>
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/shop' component={Shop} />
+      </Switch>
     </div>
   )
 }
